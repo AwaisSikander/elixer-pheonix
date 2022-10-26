92d9b61b065546bdd09d51b48ef4f7081e9d7aea
Awais Sikander <AwaisRegbits>
2022-10-26 11:08:18 +0500

    Fixed Few Errors & Defined Dependencies

--- test/password_generator_test.exs
+++ test/password_generator_test.exs
@@ -5,15 +5,15 @@ defmodule PasswordGeneratorTest do
   setup do
     options = %{
       "length" => "10",
-      "numbers" => "false"
-      "uppercase" => "false"
+      "numbers" => "false",
+      "uppercase" => "false",
       "symbols" => "false"
     }
 
     options_type = %{
-      lowercase: Enum.map(?a..?z & <<&1>>),
-      numbers: Enum.map(0..9 & Integer.to_string(&1)),
-      uppercase: Enum.map(?A..?Z & <<&1>>),
+      lowercase: Enum.map(?a..?z, & <<&1>>),
+      numbers: Enum.map(0..9, & Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, & <<&1>>),
       symbols: String.split("!#$%&()*+@-<,>./?;:[]_{}~", "", trim: true)
     }
 
