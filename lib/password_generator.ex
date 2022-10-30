066f6c54eb8783e68069ecb17ca1337075684ff8
AwaisSikander <sikande007@gmail.com>
2022-10-30 16:30:36 +0500

    Fixed All Test Casses

--- lib/password_generator.ex
+++ lib/password_generator.ex
@@ -20,7 +20,7 @@ defmodule PasswordGenerator do
         "uppercase" => "false",
         "symbols" => "false",
       }
-
+  
       iex> PasswordGenerator.generate(options)
       "asdf"
   ## Example
@@ -30,22 +30,22 @@ defmodule PasswordGenerator do
         "uppercase" => "false",
         "symbols" => "false",
       }
-
+  
       iex> PasswordGenerator.generate(options)
       "asdf453"
   """
   @spec generate(options :: map()) :: {:ok, bitstring()} | {:error, bitstring()}
   def generate(options) do
     length = Map.has_key?(options, "length")
-    validate_length(length,options)
+    validate_length(length, options)
   end
-  defp validate_length(false, _options ), do: {:error, "Please provide a length"}
 
-  defp validate_length(true, options ) do
-    numbers = Enum.map(0..9, & Integer.to_string(&1))
+  defp validate_length(false, _options), do: {:error, "Please provide a length"}
+
+  defp validate_length(true, options) do
+    numbers = Enum.map(0..9, &Integer.to_string(&1))
     length = options["length"]
-    length = String.contains(length, numbers)
-
+    length = String.contains?(length, numbers)
     validate_length_is_integer(length, options)
   end
 
@@ -54,20 +54,73 @@ defmodule PasswordGenerator do
   end
 
   defp validate_length_is_integer(true, options) do
-   length = options["length"] |> String.trim() |> String.to_integer()
-  options_without_length = Map.delete(options, "length")
-  options_values = Map.values(options_without_length)
+    length = options["length"] |> String.trim() |> String.to_integer()
+    options_without_length = Map.delete(options, "length")
+    options_values = Map.values(options_without_length)
 
-  value =
-    options_values
-    |> Enum.all?(fn x -> x |> String.to_atoms() |> is_boolean() end)
+    value =
+      options_values
+      |> Enum.all?(fn x -> String.to_atom(x) |> is_boolean() end)
 
-  validate_options_values_are_boolean(value, length, options_without_length)
- end
+    validate_options_values_are_boolean(value, length, options_without_length)
+  end
 
- defp validate_options_values_are_boolean(false, _length, _options) do
+  defp validate_options_values_are_boolean(false, _length, _options) do
     {:error, "Only Booleans allowed for options values"}
- end
+  end
 
+  defp validate_options_values_are_boolean(true, length, options) do
+    options = included_options(options)
+    invalid_options? = options |> Enum.any?(&(&1 not in @allowed_options))
+    validate_options(invalid_options?, length, options)
+  end
 
+  defp validate_options(true, _length, _options) do
+    {:error, "Only options allowed numbers, uppercase, symbols."}
+  end
+
+  defp validate_options(false, length, options) do
+    generate_strings(length, options)
+  end
+
+  defp generate_strings(length, options) do
+    options = [:lowercase_letter | options]
+
+    included = include(options)
+    length = length - length(included)
+    random_strings = generate_random_string(length, options)
+    strings = included ++ random_strings
+    get_results(strings)
+  end
+
+  defp get_results(strings) do
+    string =
+      strings
+      |> Enum.shuffle()
+      |> to_string()
+
+    {:ok, string}
+  end
+
+  defp generate_random_string(length, options) do
+    Enum.map(1..length, fn _ ->
+      Enum.random(options) |> get()
+    end)
+  end
+
+  defp include(options) do
+    options
+    |> Enum.map(&get(&1))
+  end
+
+  defp get(:lowercase_letter) do
+    <<Enum.random(?a..?z)>>
+  end
+
+  defp included_options(options) do
+    Enum.filter(options, fn {_key, value} ->
+      value |> String.trim() |> String.to_existing_atom()
+    end)
+    |> Enum.map(fn {key, _value} -> String.to_atom(key) end)
+  end
 end
